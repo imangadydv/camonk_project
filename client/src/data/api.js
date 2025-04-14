01980e6553f8e721e0a14285a22bdbc5a4905e60
imangadydv <angad310501@gmail.com>
2025-04-14 18:04:44 +0530

    fixed joining

--- client/src/data/api.js
+++ client/src/data/api.js
@@ -1,6 +1,6 @@
 export const getQuestions = async () => {
     try {
-      const res = await fetch('http://localhost:5000/questions');
+      const res = await fetch('https://camonk-backend.onrender.com/questions');
       const data = await res.json();
 
       console.log("data coming is======"+data)
