893750e84091f971d777915b90fc188bd2f851fb
imangadydv <angad310501@gmail.com>
2025-04-14 18:01:04 +0530

    fixed server

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-const PORT = process.env.PORT || 5000
+const PORT = 5000;
 
 server.use(middlewares)
 server.use(router)
